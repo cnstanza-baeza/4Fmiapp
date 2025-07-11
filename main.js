9de6d6373d20a7339dbb151d43e0ebcf4aca6fef
Jeremy2008 <jeremyjaimes@insucodos.cl>
2025-07-11 11:37:48 -0400

    Feature: Create post

---
+++ main.js
@@ -0,0 +1,23 @@
+const formulario = document.querySelector(".form-create");
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const publicacion = document.querySelector("#publicacion").value;
+    const contenedor = document.querySelector(".contenedor-publicaciones");
+
+    // Crear nuevo contenedor para la publicación
+    const nuevaPublicacion = document.createElement("div");
+    nuevaPublicacion.classList.add("publicacion"); // Clase diferente para evitar conflictos
+
+    // Agregar contenido
+    nuevaPublicacion.innerHTML = `
+        <p>Id: 00</p>
+        <p>${publicacion}</p>
+        <button class="btn-editar">Editar</button>
+        <button class="btn-eliminar">Eliminar</button>
+    `;
+
+    // Agregar al contenedor principal
+    contenedor.appendChild(nuevaPublicacion);
+});
